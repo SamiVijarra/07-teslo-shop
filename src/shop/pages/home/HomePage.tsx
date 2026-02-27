d78be8a29500d26072fb241969bc2712a2616e59
SamiVijarra <samivijarra1@gmail.com>
2026-02-27 03:03:18 -0300

    homePage

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,15 @@
 
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductGrid } from "@/shop/components/ProductsGrid"
 
 export const HomePage = () => {    
     return (
         <div>
             <CustomJumbotron title="Todos los productos"/>
+            <ProductGrid products={products}/>
+            <CustomPagination totalPages={7}/>
         </div>
     )
 }
